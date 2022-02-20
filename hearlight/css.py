04698771757c5b7fc027c62227e1153fe3c16900
Ryan Greer <ryan_greer_58@hotmail.co.uk>
2022-02-20 14:30:22 +0000

    Initial commit

---
+++ hearlight/css.py
@@ -0,0 +1,11 @@
+css = f"""
+<style>
+    .led_grid{{
+        display : grid;
+        grid-template-rows : repeat(10);
+        grid-template-columns : repeat(10):
+        height : 400px;
+        width : 400px;
+    }}
+</style>
+"""
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hearlight",
+    version="1.0.0",
+    license="BSD",
+    author="Ryan Greer, University of Strathclyde",
+    author_email="ryan.greer@strath.ac.uk",
+    packages=find_packages()
+)
